b89c0bc7c04935d6d284f3d23a5152cca2c209c2
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-04-28 20:48:00 -0300

    feat: Jordans object

---
+++ catalogo-drip-jordans/src/mocks/Jordans.js
@@ -0,0 +1,26 @@
+export const Jordans = [
+    {
+        titulo: "Nike Air Jordan- Branco sujo",
+        descricao:
+          "Quam vulputate dignissim suspendisse in est ante in nibh mauris.",
+        foto: require("assets/images/air-jordan-branco-sujo.jpg"),
+        ano:  2017,
+        preco: 400
+      },
+      {
+        titulo: "Nike Air Jordan- Letterman",
+        descricao:
+          "Quam vulputate dignissim suspendisse in est ante in nibh mauris.",
+        foto: require("assets/images/air-jordan-letterman.jpg"),
+        ano:  2016,
+        preco: 800
+      },
+      {
+        titulo: "Nike Air Jordan- Off-White",
+        descricao:
+          "Quam vulputate dignissim suspendisse in est ante in nibh mauris.",
+        foto: require("assets/images/off-white-air-jordan.jpg"),
+        ano:  2020,
+        preco: 700
+      },
+]
